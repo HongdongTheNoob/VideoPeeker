6e1d5e84d7e66e05cded9dd1a70fe3e197479a84
Qin Hongdong <hongdongdonald@gmail.com>
2024-01-31 17:00:10 +0800

    EIP sim

--- TestMain.py
+++ TestMain.py
@@ -15,26 +15,27 @@ def replace_extension(file_path, new_extension):
 if __name__ == "__main__":
   file_path = "D:/Data/xcy_test/ClassC/BasketballDrill_832x480_50.yuv"
   my_video = VideoInformation(file_path, 832, 480, 8)
-  dimensions = (64, 64, 64, 64)
+  dimensions = (320, 64, 64, 64)
+  position_string = '_(' + str(dimensions[0]) + ',' + str(dimensions[1]) + ')_' + str(dimensions[2]) + 'x' + str(dimensions[3])
 
   block, template = GetBlock.get_block(my_video, 0, dimensions, 'y', 12)
-  cv2.imwrite(replace_extension(file_path, '_y_(64,64)_64x64.png'), block)
-  cv2.imwrite(replace_extension(file_path, '_y_template_(64,64)_64x64_12.png'), template)
+  cv2.imwrite(replace_extension(file_path, '_y' + position_string + '.png'), block)
+  cv2.imwrite(replace_extension(file_path, '_y_template' + position_string + '_12.png'), template)
 
   block, template = GetBlock.get_block(my_video, 0, dimensions, 'cb', 12)
-  cv2.imwrite(replace_extension(file_path, '_cb_(64,64)_64x64.png'), block)
-  cv2.imwrite(replace_extension(file_path, '_cb_template_(64,64)_64x64_12.png'), template)
+  cv2.imwrite(replace_extension(file_path, '_cb' + position_string + '.png'), block)
+  cv2.imwrite(replace_extension(file_path, '_cb_template' + position_string + '_12.png'), template)
 
   block, template = GetBlock.get_block(my_video, 0, dimensions, 'cr', 12)
-  cv2.imwrite(replace_extension(file_path, '_cr_(64,64)_64x64.png'), block)
-  cv2.imwrite(replace_extension(file_path, '_cr_template_(64,64)_64x64_12.png'), template)
+  cv2.imwrite(replace_extension(file_path, '_cr' + position_string + '.png'), block)
+  cv2.imwrite(replace_extension(file_path, '_cr_template' + position_string + '_12.png'), template)
 
   predicted_cb_block, predicted_cr_block, x_cb, x_cr, sad_cb, sad_cr = CCCMSim.simulate_cccm(my_video, 0, dimensions, 6)
-  cv2.imwrite(replace_extension(file_path, '_cb_predicted_(64,64)_64x64.png'), predicted_cb_block)
-  cv2.imwrite(replace_extension(file_path, '_cr_predicted_(64,64)_64x64.png'), predicted_cr_block)
+  cv2.imwrite(replace_extension(file_path, '_cb_predicted' + position_string + '.png'), predicted_cb_block)
+  cv2.imwrite(replace_extension(file_path, '_cr_predicted' + position_string + '.png'), predicted_cr_block)
   print(sad_cb, sad_cr)
 
   predicted_block, coeffs, sad = EIPSim.simulate_eip(my_video, 0, dimensions, 6)
-  cv2.imwrite(replace_extension(file_path, '_y_EIP_(64,64)_64x64.png'), predicted_block)
+  cv2.imwrite(replace_extension(file_path, '_y_EIP' + position_string + '.png'), predicted_block)
   print(coeffs)
   print(sad)
