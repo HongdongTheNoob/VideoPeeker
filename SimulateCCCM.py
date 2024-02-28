8433c06490863e1aa8abffb02c2d0984983de679
Qin Hongdong <hongdongdonald@gmail.com>
2024-02-28 18:36:45 +0800

    update cccm simulations

--- SimulateCCCM.py
+++ SimulateCCCM.py
@@ -81,8 +81,15 @@ def simulation_use_case():
   print("Cr coeffs: ", x_cr0.reshape(1, -1), x_cr1.reshape(1, -1))
 
 def simulate_mmlm_looped():
-  video_class = "D"
-  qps = ["22", "27", "32", "37"]
+  video_class = "E"
+  qps = ["22"]
+
+  test_lbccp = True
+  test_l2_regularisation = True
+  l2_lambdas = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
+  test_soft_mmlm = True
+
+  load_block_stats = False
 
   for i in range(len(VideoDataset.video_sequences[video_class])):
     sequence = VideoDataset.video_sequences[video_class][i]
@@ -98,20 +105,22 @@ def simulate_mmlm_looped():
       if not bms_files:
         continue
 
-      all_blocks = BmsStatsScanner.collect_blocks(bms_files[0], frame_range = range(1), target_string = "Chroma_IntraMode=68")
-      with open("./Tests/MMLM_Sim/" + sequence + "-" + qp + ".json", "w") as file:
-        json.dump(all_blocks, file)    
-      # with open("./Tests/MMLM_Sim/" + sequence + "-" + qp + ".json", "r") as file:
-      #   all_blocks = json.load(file)    
+      if not load_block_stats:
+        all_blocks = BmsStatsScanner.collect_blocks(bms_files[0], frame_range = range(1), target_string = "Chroma_IntraMode=68")
+        with open("./Tests/MMLM_Sim/" + sequence + "-" + qp + ".json", "w") as file:
+          json.dump(all_blocks, file)   
+      else: 
+        with open("./Tests/MMLM_Sim/" + sequence + "-" + qp + ".json", "r") as file:
+          all_blocks = json.load(file)    
 
       # Loop
       pixel_count = 0
       total_sad_mmlm = 0
       overall_sad_change_lbccp = 0
-      overall_sad_change_l2 = 0
-      overall_sad_change_soft = 0
       overall_sad_gain_lbccp = 0
-      overall_sad_gain_l2 = 0
+      overall_sad_change_l2 = [0 for _ in l2_lambdas]
+      overall_sad_gain_l2 = [0 for _ in l2_lambdas]
+      overall_sad_change_soft = 0
       overall_sad_gain_soft = 0
 
       mmlm_test_log_file = open("./Tests/MMLM_Sim/" + sequence + "-" + qp + ".txt", "w")
@@ -129,45 +138,55 @@ def simulate_mmlm_looped():
         predicted_cb_block, predicted_cr_block, sad_cb, sad_cr, coeffs, mad = CCCMSim.simulate_mm_cccm(my_video, 0, dimensions, 6)
         sad_mmlm = sad_cb + sad_cr
         mmlm_test_log_file.write(" ".join(["SADs MM-CCCM: ", str(sad_cb), str(sad_cr), "\n"]))
-        # lbccp
-        lbccp_kernel = np.array([[1, 1, 1], [1, 8, 1], [1, 1, 1]]) / 16
-        filtered_cb_block = Filters.apply_filter(my_video, frame, dimensions, 'cb', predicted_cb_block, lbccp_kernel)
-        filtered_cr_block = Filters.apply_filter(my_video, frame, dimensions, 'cr', predicted_cr_block, lbccp_kernel)
-        sad_cb = np.sum(np.abs(filtered_cb_block.astype("int32") - cb_block))
-        sad_cr = np.sum(np.abs(filtered_cr_block.astype("int32") - cr_block))
-        sad_mmlm_lbccp = sad_cb + sad_cr
-        mmlm_test_log_file.write(" ".join(["SADs MM-CCCM with LBCCP: ", str(sad_cb), str(sad_cr), " change ", str(sad_mmlm_lbccp - sad_mmlm), "\n"]))
-
-        predicted_cb_block, predicted_cr_block, sad_cb, sad_cr, coeffs, mad = CCCMSim.simulate_mm_cccm(my_video, 0, dimensions, 6, l2_regularisation = 100)
-        sad_mmlm_l2_100 = sad_cb + sad_cr
-        mmlm_test_log_file.write(" ".join(["SADs MM-CCCM-L2-100: ", str(sad_cb), str(sad_cr), " change ", str(sad_mmlm_l2_100 - sad_mmlm), "\n"]))
-
-        predicted_cb_block, predicted_cr_block, sad_cb, sad_cr, coeffs = CCCMSim.simulate_soft_classified_mm_cccm(my_video, 0, dimensions, 6)
-        sad_new_mmlm = sad_cb + sad_cr
-        mmlm_test_log_file.write(" ".join(["SADs Soft MM-CCCM: ", str(sad_cb), str(sad_cr), " change ", str(sad_new_mmlm - sad_mmlm), "\n"]))
-
         pixel_count += dimensions[2] * dimensions[3] // 4
         total_sad_mmlm += sad_mmlm
-        overall_sad_change_lbccp += sad_mmlm_lbccp - sad_mmlm
-        overall_sad_change_l2 += sad_mmlm_l2_100 - sad_mmlm
-        overall_sad_change_soft += sad_new_mmlm - sad_mmlm
-        overall_sad_gain_lbccp += max(0, sad_mmlm - sad_mmlm_lbccp)
-        overall_sad_gain_l2 += max(0, sad_mmlm - sad_mmlm_l2_100)
-        overall_sad_gain_soft += max(0, sad_mmlm - sad_new_mmlm)
+
+        # lbccp
+        if test_lbccp:
+          lbccp_kernel = np.array([[1, 1, 1], [1, 8, 1], [1, 1, 1]]) / 16
+          filtered_cb_block = Filters.apply_filter(my_video, frame, dimensions, 'cb', predicted_cb_block, lbccp_kernel)
+          filtered_cr_block = Filters.apply_filter(my_video, frame, dimensions, 'cr', predicted_cr_block, lbccp_kernel)
+          sad_cb = np.sum(np.abs(filtered_cb_block.astype("int32") - cb_block))
+          sad_cr = np.sum(np.abs(filtered_cr_block.astype("int32") - cr_block))
+          sad_mmlm_lbccp = sad_cb + sad_cr
+          mmlm_test_log_file.write(" ".join(["SADs MM-CCCM with LBCCP: ", str(sad_cb), str(sad_cr), " change ", str(sad_mmlm_lbccp - sad_mmlm), "\n"]))
+          overall_sad_change_lbccp += sad_mmlm_lbccp - sad_mmlm
+          overall_sad_gain_lbccp += max(0, sad_mmlm - sad_mmlm_lbccp)
+
+        if test_l2_regularisation:
+          for i in range(len(l2_lambdas)):
+            predicted_cb_block, predicted_cr_block, sad_cb, sad_cr, coeffs, mad = CCCMSim.simulate_mm_cccm(my_video, 0, dimensions, 6, l2_regularisation = l2_lambdas[i])
+            sad_mmlm_l2 = sad_cb + sad_cr
+            mmlm_test_log_file.write(" ".join(["SADs MM-CCCM-L2-100: ", str(sad_cb), str(sad_cr), " change ", str(sad_mmlm_l2 - sad_mmlm), "\n"]))
+            overall_sad_change_l2[i] += sad_mmlm_l2 - sad_mmlm
+            overall_sad_gain_l2[i] += max(0, sad_mmlm - sad_mmlm_l2)
+
+        if test_soft_mmlm:
+          predicted_cb_block, predicted_cr_block, sad_cb, sad_cr, coeffs = CCCMSim.simulate_soft_classified_mm_cccm(my_video, 0, dimensions, 6)
+          sad_new_mmlm = sad_cb + sad_cr
+          mmlm_test_log_file.write(" ".join(["SADs Soft MM-CCCM: ", str(sad_cb), str(sad_cr), " change ", str(sad_new_mmlm - sad_mmlm), "\n"]))
+          overall_sad_change_soft += sad_new_mmlm - sad_mmlm
+          overall_sad_gain_soft += max(0, sad_mmlm - sad_new_mmlm)
+
       
       print(sequence, qp)
       print("Pixel count: ", pixel_count)
       print("MMLM total SAD: ", total_sad_mmlm)
-      print("Overall SAD change LBCCP: ", overall_sad_change_lbccp)
-      print("Overall SAD change L2 Regularisation: ", overall_sad_change_l2)
-      print("Overall SAD change soft MMLM: ", overall_sad_change_soft)
-      print("Overall SAD gain LBCCP: ", overall_sad_gain_lbccp)
-      print("Overall SAD gain L2 Regularisation: ", overall_sad_gain_l2)
-      print("Overall SAD gain soft MMLM: ", overall_sad_gain_soft)
+      if test_lbccp:
+        print("Overall SAD change LBCCP: ", overall_sad_change_lbccp)
+        print("Overall SAD gain LBCCP: ", overall_sad_gain_lbccp)
+      if test_l2_regularisation:
+        for i in range(len(l2_lambdas)):
+          print("Overall SAD change L2 lambda =", str(l2_lambdas[i]), ": ", overall_sad_change_l2[i])
+        for i in range(len(l2_lambdas)):
+          print("Overall SAD gain L2 lambda =", str(l2_lambdas[i]), ": ", overall_sad_gain_l2[i])
+      if test_soft_mmlm:
+        print("Overall SAD change soft MMLM: ", overall_sad_change_soft)
+        print("Overall SAD gain soft MMLM: ", overall_sad_gain_soft)
 
 
 def simulate_cccm_looped():
-  video_class = "C"
+  video_class = "D"
   qps = ["22", "27", "32", "37"]
 
   for i in range(len(VideoDataset.video_sequences[video_class])):
@@ -179,7 +198,7 @@ def simulate_cccm_looped():
     video_folder = os.path.dirname(file_path)
 
     for qp in qps:
-      bms_files = VideoDataset.find_stats_files(VideoDataset.bms_file_base_path, sequence, qp)
+      bms_files = VideoDataset.find_stats_files(os.path.join(VideoDataset.bms_file_base_path, 'Class' + video_class), sequence, qp)
 
       if not bms_files:
         continue
