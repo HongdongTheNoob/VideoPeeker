77f4018168f064969838282b2d1c04a47e98715a
Qin Hongdong <hongdong.qin@tcl.com>
2024-08-20 17:34:40 +0800

    Add some Pdp Visualisation

--- MIP/PrintMip.py
+++ MIP/PrintMip.py
@@ -1,51 +1,72 @@
 import numpy as np
 import MipData
+import PdpData
 
 numpy_arrays = np.array(MipData.data16x16)
 
 for sample in range(0, 7):
-    extract_element = numpy_arrays[:, :, sample]  # Extract the element corresponding to n-th sample along axis 0
-    extract_element = extract_element.reshape((-1, 8, 8))
-    for matrix in extract_element:
-        for row in matrix:
-            print(','.join(map(str, row)))
-        print()
+  extract_element = numpy_arrays[:, :, sample]  # Extract the element corresponding to n-th sample along axis 0
+  extract_element = extract_element.reshape((-1, 8, 8))
+  for matrix in extract_element:
+  for row in matrix:
+    print(','.join(map(str, row)))
+  print()
 
 def ComputeMIP(reducedBoundary, dataMatrix):
-    dataArrays = np.array(dataMatrix)
+  dataArrays = np.array(dataMatrix)
 
-    pTemp0 = reducedBoundary[0]
+  pTemp0 = reducedBoundary[0]
 
-    if(dataArrays.shape[2] == 7):
-        reducedBoundary[1:] = [x - reducedBoundary[0] for x in reducedBoundary[1:]]
-        reducedBoundary.pop(0)
+  if(dataArrays.shape[2] == 7):
+    reducedBoundary[1:] = [x - reducedBoundary[0] for x in reducedBoundary[1:]]
+    reducedBoundary.pop(0)
+  else:
+    reducedBoundary[1:] = [x - reducedBoundary[0] for x in reducedBoundary[1:]]
+    reducedBoundary[0] = 128 - reducedBoundary[0]
+
+  reducedBoundary = np.array(reducedBoundary)
+
+  oW = 32 - 32 * sum(reducedBoundary)
+
+  returnValues = []
+  for i in range(0, dataArrays.shape[0]):
+    matrix = dataArrays[i, :, :]
+    output = matrix.dot(reducedBoundary)
+    output += oW
+    output //= 64
+    output += pTemp0
+    if(output.size == 16):
+      output = output.reshape(-1, 4, 4)
     else:
-        reducedBoundary[1:] = [x - reducedBoundary[0] for x in reducedBoundary[1:]]
-        reducedBoundary[0] = 128 - reducedBoundary[0]
+      output = output.reshape(-1, 8, 8)
+    returnValues.append(output)
 
-    reducedBoundary = np.array(reducedBoundary)
+  return returnValues
 
-    oW = 32 - 32 * sum(reducedBoundary)
+def ComputePDP(referenceSamples, weightMatrix, modeIndex):
+  # convert mode index
 
-    returnValues = []
-    for i in range(0, dataArrays.shape[0]):
-        matrix = dataArrays[i, :, :]
-        output = matrix.dot(reducedBoundary)
-        output += oW
-        output //= 64
-        output += pTemp0
-        if(output.size == 16):
-            output = output.reshape(-1, 4, 4)
-        else:
-            output = output.reshape(-1, 8, 8)
-        returnValues.append(output)
 
-    return returnValues
+  weightArray = np.array(weightMatrix[modeId])
+
+    # int sum = 0;
+    # const int16_t* f = filter[y >> yShift][(x >> xShift)>>2];
+    # for (int i = 0; i < refLen; ++i)
+    # {
+    # auto xs = x>>xShift;
+    # sum += ref[i] * f[(i / 8) * 32 + (xs % 4) * 8 + ( i % 8 )];
+    # }
+    # pred[x] = (Pel)Clip3(clpRng.min, clpRng.max, (std::max(0, sum) + addShift) >> 14);
+
+  output = weightArray.mul(referenceSamples)
+
+
+  return returnValues
 
 # boundary = [138, 128, 128, 128]
 # predictors = ComputeMIP(boundary, data4x4)
 # for p in predictors:
-#     for row in p:
-#         for elem in row:
-#             print("{}".format(elem).rjust(5), end="\n")
-#     print()
+#   for row in p:
+#     for elem in row:
+#       print("{}".format(elem).rjust(5), end="\n")
+#   print()
---
+++ VisualisePdpWeights.py
@@ -0,0 +1,100 @@
+import os
+import sys
+import cv2
+import numpy as np
+import json
+import csv
+import MIP.PdpData
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.patches import Rectangle
+
+blockWidth = 8
+blockHeight = 8
+referenceLines = 2
+
+blockWithReferenceSamples = np.full((blockHeight * 2 + referenceLines, blockWidth * 2 + referenceLines), 128)
+invocation_type = 'left_bar1'
+
+if invocation_type == 'corner_topleft':
+  blockWithReferenceSamples[0:2,0:2] = 192
+if invocation_type == 'top_bar':
+  blockWithReferenceSamples[0:2,:] = 192
+if invocation_type == 'top_bar0':
+  blockWithReferenceSamples[1,:] = 192
+if invocation_type == 'top_bar1':
+  blockWithReferenceSamples[0,:] = 192
+if invocation_type == 'left_bar':
+  blockWithReferenceSamples[:,0:2] = 192
+if invocation_type == 'left_bar0':
+  blockWithReferenceSamples[:,1] = 192
+if invocation_type == 'left_bar1':
+  blockWithReferenceSamples[:,0] = 192
+
+# Long reference samples
+stride = blockWidth * 2 + referenceLines
+referenceLength = (blockWidth * 2 + referenceLines) * referenceLines + blockHeight * 2 * referenceLines
+referenceSamples = np.zeros(referenceLength)
+
+# Fill reference samples
+for i in range(referenceLines):
+  referenceSamples[(i*stride):((i+1)*stride)] = blockWithReferenceSamples[i,:]
+for i in range(blockHeight*2):
+  referenceSamples[referenceLines*stride+i*referenceLines:referenceLines*stride+(i+1)*referenceLines] = blockWithReferenceSamples[i+referenceLines,0:referenceLines]
+
+referenceSamples = np.reshape(referenceSamples, (referenceLength, 1))
+for modeId in range(11):
+
+  weightMatrix = np.array(MIP.PdpData.g_weights8x8[modeId])
+
+  # Compute matrix
+  outputBlock = np.matmul(weightMatrix, referenceSamples) // np.power(2, 14)
+  outputBlock = np.reshape(outputBlock, (blockHeight, blockWidth))
+
+  # Fill back
+  blockWithReferenceSamples[referenceLines:referenceLines+blockHeight,referenceLines:referenceLines+blockWidth] = outputBlock
+
+  # Display
+  plt.imshow(blockWithReferenceSamples, cmap='gray', interpolation='nearest', vmin=0, vmax=255)
+  rect = Rectangle((referenceLines-0.5, referenceLines-0.5), blockWidth, blockHeight,
+                  linewidth=2, edgecolor='black', facecolor='none')
+  plt.gca().add_patch(rect)
+  # plt.show()
+
+  modeNumber = (modeId * 2 - 2) if modeId > 1 else modeId
+  fileName = f'./MIP/PdpVisualisation/{invocation_type}_{modeNumber:02d}.png'
+  plt.savefig(fileName, dpi=300, bbox_inches='tight')
+
+# Short reference samples
+stride = blockWidth + referenceLines
+referenceLength = (blockWidth + referenceLines) * referenceLines + blockHeight * referenceLines
+referenceSamples = np.zeros(referenceLength)
+
+# Fill reference samples
+for i in range(referenceLines):
+  referenceSamples[(i*stride):((i+1)*stride)] = blockWithReferenceSamples[i,0:stride]
+for i in range(blockHeight):
+  referenceSamples[referenceLines*stride+i*referenceLines:referenceLines*stride+(i+1)*referenceLines] = blockWithReferenceSamples[i+referenceLines,0:referenceLines]
+
+referenceSamples = np.reshape(referenceSamples, (referenceLength, 1))
+for modeId in range(8):
+
+  weightMatrix = np.array(MIP.PdpData.g_weightsShort8x8[modeId])
+
+  # Compute matrix
+  outputBlock = np.matmul(weightMatrix, referenceSamples) // np.power(2, 14)
+  outputBlock = np.reshape(outputBlock, (blockHeight, blockWidth))
+
+  # Fill back
+  blockWithReferenceSamples[referenceLines:referenceLines+blockHeight,referenceLines:referenceLines+blockWidth] = outputBlock
+
+  # Display
+  plt.imshow(blockWithReferenceSamples, cmap='gray', interpolation='nearest', vmin=0, vmax=255)
+  rect = Rectangle((referenceLines-0.5, referenceLines-0.5), blockWidth, blockHeight,
+                  linewidth=2, edgecolor='black', facecolor='none')
+  plt.gca().add_patch(rect)
+  # plt.show()
+
+  modeNumber = modeId * 2 + 20
+  fileName = f'./MIP/PdpVisualisation/{invocation_type}_{modeNumber:02d}.png'
+  plt.savefig(fileName, dpi=300, bbox_inches='tight')
