27a4845803083ec2a298c847b97d1330b73033a6
Qin Hongdong <hongdong.qin@tcl.com>
2024-08-30 17:51:32 +0800

    Developing IPM

--- IPM/IPMSim.py
+++ IPM/IPMSim.py
@@ -1,5 +1,5 @@
 import numpy as np
-import IntraFilters
+from . import IntraFilters
 
 angTable = [ 
    0,    1,    2,    3,    4,    6,    8,   10,   
@@ -8,8 +8,10 @@ angTable = [
   86,  102,  128,  171,  256,  341,  512, 1024 
 ]
 invAngTable = [
-  0,   16384, 8192, 5461, 4096, 2731, 2048, 1638, 1365, 1170, 1024, 910, 819, 712, 630, 565,
-  512, 468,   420,  364,  321,  287,  256,  224,  191,  161,  128,  96,  64,  48,  32,  16
+  0,   16384, 8192, 5461, 4096, 2731, 2048, 1638, 
+  1365, 1170, 1024, 910, 819, 712, 630, 565,
+  512, 468,   420,  364,  321,  287,  256,  224,  
+  191,  161,  128,  96,  64,  48,  32,  16
 ]
 
 def FillMainReferenceLine(blockWidthReferenceLine, blockSize, modeId, filterTaps = 6):
@@ -17,21 +19,23 @@ def FillMainReferenceLine(blockWidthReferenceLine, blockSize, modeId, filterTaps
   intraAnglePredMode = modeId - 50
 
   # Top part
-  middlePart = blockWidthReferenceLine[0, 0:blockWidth*2+1]
+  middlePart = np.array(blockWidthReferenceLine[0, 0:blockWidth*2+1]).reshape((1, blockWidth*2+1))
 
   # Extend to the right
-  extendedRight = [middlePart[-1]] * filterTaps
+  extendedRight = np.full((1, filterTaps), middlePart[0, -1])
 
   # Left part
   leftPart = np.zeros((1, blockHeight + 1))
 
+  print(modeId, leftPart.shape, middlePart.shape, extendedRight.shape)
+
   if intraAnglePredMode >= 0:
     maxLeftOffset = (filterTaps - 1) // 2
-    leftPart[-(maxLeftOffset-1):0] = middlePart[0]
+    leftPart[-(maxLeftOffset-1):0] = middlePart[0, 0]
   else:
     for deltaX in range(-blockHeight+1, 0):
       idx = blockHeight + 1 + deltaX
-      pixelPosition = (-deltaX * invAngTable[intraAnglePredMode] + 8) // 16
+      pixelPosition = (-deltaX * invAngTable[abs(intraAnglePredMode)] + 8) // 16
       integerPel = pixelPosition // 32
       fractionalPel = pixelPosition % 32
 
@@ -67,13 +71,13 @@ def PredIntraAngular(blockWidthReferenceLine, blockSize, modeId):
   
   mainReferenceLine = FillMainReferenceLine(blockWidthReferenceLine, blockSize, predModeIndex)
 
-  yy, xx = np.meshgrid([blockHeight, blockWidth], indexing='ij')
+  yy, xx = np.meshgrid(blockHeight, blockWidth, indexing='ij')
 
   referenceLinePosition = (xx * 32) + yy * predModeSign * angTable[abs(predModeIndex - 50)]
   referenceLinePosition = referenceLinePosition + ((blockHeight + 1) * 32)
   referenceLinePositionInteger = referenceLinePosition // 32
   referenceLinePositionFractional = referenceLinePosition % 32
-   
+
   # const TFilterCoeff* const f = (useCubicFilter) ? ( bExtIntraDir ? InterpolationFilter::getIntraLumaFilterTableExt(deltaFract) : InterpolationFilter::getIntraLumaFilterTable(deltaFract)) : (width >=32 && height >=32)? (bExtIntraDir ? intraSmoothingFilter2Ext : intraSmoothingFilter2) : (bExtIntraDir ? intraSmoothingFilterExt : intraSmoothingFilter);
   # if useCubicFilter: !m_ipaParam.interpolationFlag >> reference Filter >> integer slope modes >> angTable value is multiples of 32
   #   InterpolationFilter::getIntraLumaFilterTable(deltaFract)
@@ -105,4 +109,4 @@ def PredIntraAngular(blockWidthReferenceLine, blockSize, modeId):
     predictionBlock += np.multiply(predictionFilter[0], mainReferenceLine[referenceLinePositionInteger])
   predictionBlock = predictionBlock // 128
 
-
+  return predictionBlock
--- VisualiseIPM.py
+++ VisualiseIPM.py
@@ -12,3 +12,39 @@ from scipy.ndimage import zoom
 import IPM.IPMSim as IPMSim
 import FillReferencePatterns
 
+blockSizes = [
+  # (4, 4), 
+  # (4, 8), 
+  # (8, 4), 
+  # (4, 16), 
+  # (16, 4), 
+  # (8, 8), 
+  # (8, 16), 
+  # (16, 8), 
+  (16, 16), 
+  # (16, 32), 
+  # (32, 16), 
+  # (32, 32),
+]
+
+for blockSize in blockSizes:
+  blockWidth, blockHeight = blockSize
+  RL = 1
+  
+  saveFolder = f'./IPM/IpmVisualisation/{blockWidth}x{blockHeight}'
+  if not os.path.exists(saveFolder):
+    os.mkdir(saveFolder)
+    
+  for invocationType in FillReferencePatterns.invocationTypes:
+    if RL == 1:
+      if invocationType in ['top_bar0', 'top_bar1', 'left_bar0', 'left_bar1']:
+        continue
+    
+    print(f'Simulating: {blockWidth}x{blockHeight}', invocationType)
+    blockWithReferenceSamples = np.full((blockHeight * 2 + RL, blockWidth * 2 + RL), 128)
+    blockWithReferenceSamples = FillReferencePatterns.FillReferenceSamples(blockWithReferenceSamples, blockSize, invocationType)
+
+    for modeId in range(2, 67):
+      predictionBlock = IPMSim.PredIntraAngular(blockWithReferenceSamples, blockSize, modeId)
+      blockWithReferenceSamples[RL:RL+blockHeight, RL:RL+blockWidth] = predictionBlock
+
